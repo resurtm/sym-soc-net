4a47714474689aabe562dd268daa4386aeaae183
resurtm <resurtm@gmail.com>
2017-01-05 14:21:02 +0600

    login form

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -13,7 +13,15 @@ class SecurityController extends Controller
      */
     public function loginAction(Request $request)
     {
-        return $this->render('auth/login.html.twig');
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
     /**
@@ -21,6 +29,6 @@ class SecurityController extends Controller
      */
     public function signupAction(Request $request)
     {
-        return $this->render('auth/signup.html.twig');
+        return $this->render('security/signup.html.twig');
     }
 }
