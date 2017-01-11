f985ba8165aa9932f2c8896d27c5774cb4051ede
resurtm <resurtm@gmail.com>
2017-01-11 15:09:27 +0600

    password change wip

---
+++ src/AppBundle/Cabinet/PasswordChange.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Cabinet;
+
+class PasswordChange
+{
+    /**
+     * @var string
+     */
+    private $oldPassword;
+
+    /**
+     * @var string
+     */
+    private $newPassword;
+
+    /**
+     * @return string
+     */
+    public function getOldPassword()
+    {
+        return $this->oldPassword;
+    }
+
+    /**
+     * @param string $oldPassword
+     * @return $this
+     */
+    public function setOldPassword($oldPassword)
+    {
+        $this->oldPassword = $oldPassword;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewPassword()
+    {
+        return $this->newPassword;
+    }
+
+    /**
+     * @param string $newPassword
+     * @return $this
+     */
+    public function setNewPassword($newPassword)
+    {
+        $this->newPassword = $newPassword;
+        return $this;
+    }
+}
--- src/AppBundle/Controller/CabinetController.php
+++ src/AppBundle/Controller/CabinetController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Cabinet\PasswordChange;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class CabinetController extends Controller
@@ -21,6 +25,37 @@ class CabinetController extends Controller
      */
     public function passwordAction(Request $request)
     {
-        return $this->render('cabinet/password.html.twig');
+        $passwordChange = new PasswordChange();
+
+        $form = $this->createFormBuilder($passwordChange)
+            ->add('oldPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'invalid_message' => 'The old password fields must match.',
+                'first_options'  => array('label' => 'Old Password'),
+                'second_options' => array('label' => 'Repeat Old Password'),
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'invalid_message' => 'The new password fields must match.',
+                'first_options'  => array('label' => 'New Password'),
+                'second_options' => array('label' => 'Repeat New Password'),
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Update'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $passwordChange = $form->getData();
+            $this->get('app.password_changer')->changePassword($passwordChange);
+
+            return $this->redirectToRoute('cabinet_password');
+        }
+
+        return $this->render('cabinet/password.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
